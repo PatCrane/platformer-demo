e48d5a9dfe06fff18dd7c73d34c951d48e0f5130
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-11-03 23:06:13 -0500

    bullets

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -4,13 +4,14 @@ var direction_x: float
 var speed := 50
 @export var jump_strength := 10
 @export var gravity := 10
+signal shoot(pos: Vector2, dir: Vector2)
 
 func get_input():
 	direction_x = Input.get_axis("left","right")
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_strength
 	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0:
-		print("shoot")
+		shoot.emit(position, get_local_mouse_position().normalized())
 		$ReloadTimer.start()
 
 func apply_gravity(delta):
