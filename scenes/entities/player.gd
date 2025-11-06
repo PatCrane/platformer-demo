d7d7fbece621209b1727d07b38a20d97f3ec48e3
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-11-05 22:39:33 -0500

    player movement and animations

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -6,6 +6,17 @@ var speed := 50
 @export var gravity := 10
 signal shoot(pos: Vector2, dir: Vector2)
 
+const gun_directions = {
+	Vector2i(1,0): 0,
+	Vector2i(1,1): 1,
+	Vector2i(0,1): 2,
+	Vector2i(-1,1): 3,
+	Vector2i(-1,0): 4,
+	Vector2i(-1,-1): 5,
+	Vector2i(0,-1): 6,
+	Vector2i(1,-1): 7,
+}
+
 func get_input():
 	direction_x = Input.get_axis("left","right")
 	if Input.is_action_just_pressed("jump"):
@@ -22,3 +33,16 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction_x * speed
 	apply_gravity(delta)
 	move_and_slide()
+	animation()
+	
+func animation():
+	$Legs.flip_h = direction_x < 0
+	if is_on_floor():
+		$AnimationPlayer.current_animation = 'run' if direction_x else 'idle'
+	else:
+		$AnimationPlayer.current_animation = 'jump'
+	var raw_dir = get_local_mouse_position().normalized()
+	var adjusted_dir = Vector2i(round(raw_dir.x),round(raw_dir.y))
+	$Torso.frame = gun_directions[adjusted_dir]
+	
+		
